c75a32e99065c64651f165e4dc611edc681a07c8
Geiner Grandez Valle <geiner120300@gmail.com>
2025-12-11 02:12:07 -0500

    feat: add support for multiple networks in NearDIDResolver and update version to 1.4.7

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,33 +4,34 @@ import { NearDIDResolver } from "../src";
 
 const CONTRACT_ID = "neardti.testnet";
 const RPC_URL = "https://rpc.testnet.near.org";
+const RPC_URL_MAINNET = "https://rpc.mainnet.near.org";
 
-jest.mock("near-api-js", () => {
-  const original = jest.requireActual("near-api-js");
+// jest.mock("near-api-js", () => {
+//   const original = jest.requireActual("near-api-js");
 
-  return {
-    ...original,
-    connect: jest.fn().mockResolvedValue({
-      account: async () => ({
-        viewFunction: async ({ methodName }: { methodName: string }) => {
-            if (methodName === "identity_owner") {
-              return "did:near:CF5RiJYh4EVmEt8UADTjoP3XaZo1NPWxv6w5TmkLqjpR";
-            }
-        },
-        getAccessKeys: async () => {
-          return [{access_key: {permission: "FullAccess"}, public_key: "ed25519:7WLUHT69sw5UpYK9xAY5cbdWKf4vSMruXzwfbL999zXo"}]
-        }
-      }),
-    }),
-    providers: {
-      JsonRpcProvider: jest.fn().mockImplementation(() => ({
-        query: async () => ({
-          public_key: "ed25519:AnExampleBase58KeyString",
-        }),
-      })),
-    },
-  };
-});
+//   return {
+//     ...original,
+//     connect: jest.fn().mockResolvedValue({
+//       account: async () => ({
+//         viewFunction: async ({ methodName }: { methodName: string }) => {
+//             if (methodName === "identity_owner") {
+//               return "did:near:CF5RiJYh4EVmEt8UADTjoP3XaZo1NPWxv6w5TmkLqjpR";
+//             }
+//         },
+//         getAccessKeys: async () => {
+//           return [{access_key: {permission: "FullAccess"}, public_key: "ed25519:7WLUHT69sw5UpYK9xAY5cbdWKf4vSMruXzwfbL999zXo"}]
+//         }
+//       }),
+//     }),
+//     providers: {
+//       JsonRpcProvider: jest.fn().mockImplementation(() => ({
+//         query: async () => ({
+//           public_key: "ed25519:AnExampleBase58KeyString",
+//         }),
+//       })),
+//     },
+//   };
+// });
 
 jest.mock("bs58", () => ({
     encode: jest.fn().mockImplementation((buf) => `bs58(${Buffer.from(buf).toString("hex")})`),
@@ -41,16 +42,23 @@ jest.mock("bs58", () => ({
 
 describe("resolveDID", () => {
   let resolver: NearDIDResolver;
+  let resolver_both: NearDIDResolver;
 
   beforeEach(() => {
     resolver = new NearDIDResolver(CONTRACT_ID, RPC_URL);
+    resolver_both = new NearDIDResolver({
+      networks: [
+        {contractId: CONTRACT_ID, rpcUrl: RPC_URL_MAINNET, networkId: "near"},
+        {contractId: CONTRACT_ID, rpcUrl: RPC_URL, networkId: "testnet"},
+      ],
+    });
   });
 
   it("should return a valid DID Document", async () => {
     const doc = await resolver.resolveDID("did:near:CF5RiJYh4EVmEt8UADTjoP3XaZo1NPWxv6w5TmkLqjpR");
 
     expect(doc).toEqual({
-      "@context": "https://w3id.org/did/v1",
+      "@context": doc['@context'],
       id: "did:near:CF5RiJYh4EVmEt8UADTjoP3XaZo1NPWxv6w5TmkLqjpR",
       verificationMethod: [
         {
@@ -72,7 +80,51 @@ describe("resolveDID", () => {
     const id = did + "#owner"
 
     expect(doc).toEqual({
-      "@context": "https://w3id.org/did/v1",
+      "@context": doc['@context'],
+      id: did,
+      verificationMethod: [
+        {
+          id: id,
+          type: "Ed25519VerificationKey2018",
+          controller: did,
+          publicKeyBase58: "7WLUHT69sw5UpYK9xAY5cbdWKf4vSMruXzwfbL999zXo",
+        },
+      ],
+      authentication: [id],
+      assertionMethod: [id],
+    });
+  });
+
+  it("should return a valid DID Document for named account mainnet", async () => {
+    const did = "did:near:geinergv.near";
+    const doc = await resolver_both.resolveDID(did);
+
+    const id = did + "#owner"
+
+    expect(doc).toEqual({
+      "@context": doc['@context'],
+      id: did,
+      verificationMethod: [
+        {
+          id: id,
+          type: "Ed25519VerificationKey2018",
+          controller: did,
+          publicKeyBase58: "8hagSnSS6RngSHHmSfwjqEoj3Namt1aVtwZjiJAdrEcf",
+        },
+      ],
+      authentication: [id],
+      assertionMethod: [id],
+    });
+  });
+
+  it("should return a valid DID Document for named account testnet", async () => {
+    const did = "did:near:geinergv.testnet";
+    const doc = await resolver_both.resolveDID(did);
+
+    const id = did + "#owner"
+
+    expect(doc).toEqual({
+      "@context": doc['@context'],
       id: did,
       verificationMethod: [
         {
--- src/resolver.ts
+++ src/resolver.ts
@@ -1,15 +1,46 @@
 import { DIDResolver } from 'did-resolver';
 import { Account, connect, keyStores } from "near-api-js";
 
+type NetworkConfig = {
+  rpcUrl: string;
+  networkId: string;
+  contractId: string;
+}
+
+type NearResolverOptions = {
+  networks: NetworkConfig[];
+}
+
 export class NearDIDResolver {
   private readonly NETWORK_ID: string;
   private readonly RPC_URL: string;
   private readonly CONTRACT_ID: string;
+  private readonly networks: Record<string, NetworkConfig>;
 
-  constructor(contract_id: string, rpc_url: string, network_id: string = "testnet") {
-    this.CONTRACT_ID = contract_id;
-    this.RPC_URL = rpc_url;
-    this.NETWORK_ID = network_id;
+  constructor(options: NearResolverOptions);
+  constructor(contract_id: string, rpc_url?: string, network_id?: string);
+  constructor(contract_id: string|NearResolverOptions, rpc_url: string = "", network_id: string = "testnet") {
+    if (typeof contract_id === "object") {
+      const options = contract_id;
+      this.networks = options.networks ? options.networks.reduce((n, network)=>{
+        n[network.networkId] = network;
+        return n;
+      }, {} as Record<string, NetworkConfig>) : {};
+      const defaultNetwork = options.networks[0];
+      this.CONTRACT_ID = defaultNetwork.contractId;
+      this.RPC_URL = defaultNetwork.rpcUrl;
+      this.NETWORK_ID = defaultNetwork.networkId;
+    } else {
+      this.CONTRACT_ID = contract_id;
+      this.RPC_URL = rpc_url;
+      this.NETWORK_ID = network_id;
+      this.networks = {};
+      this.networks[this.NETWORK_ID] = {
+        contractId: this.CONTRACT_ID,
+        networkId: this.NETWORK_ID,
+        rpcUrl: this.RPC_URL,
+      };
+    }
   }
 
   private isNamedAccount(did: string): boolean {
@@ -22,16 +53,16 @@ export class NearDIDResolver {
     return /^[1-9A-HJ-NP-Za-km-z]{44,50}$/.test(identifier);
   }
 
-  private async getNear() {
+  private async getNear(network: NetworkConfig) {
     return connect({
-      networkId: this.NETWORK_ID,
-      nodeUrl: this.RPC_URL,
+      networkId: network.networkId,
+      nodeUrl: network.rpcUrl,
       deps: { keyStore: new keyStores.InMemoryKeyStore() },
     });
   }
 
-  private async getPublicKeyFromRPC(accountId: string): Promise<string[]> {
-    const near = await this.getNear();
+  private async getPublicKeyFromRPC(accountId: string, network: NetworkConfig): Promise<string[]> {
+    const near = await this.getNear(network);
     const account = await near.account(accountId);
 
     if (!account) {
@@ -47,28 +78,56 @@ export class NearDIDResolver {
     return accessKeys.filter((a)=>a.access_key.permission == "FullAccess").map(a=>a.public_key.replace('ed25519:', ''))
   }
 
+  getNetworkIdFromAccountId(accountId: string): string {
+    if (accountId.endsWith('.near')) {
+      return 'near';
+    } else {
+      return 'testnet';
+    }
+  }
+
+  public getNetworkIdFromDid(did: string): string {
+    const parts = did.split(':');
+    const accountId = parts[parts.length - 1];
+    if (this.isNamedAccount(did)) {
+      return this.getNetworkIdFromAccountId(accountId);
+    } else if (parts.length === 4) {
+      return parts[2];
+    } else {
+      return this.NETWORK_ID;
+    }
+  }
+
   public async resolveDID(did: string) {
     const identifier = did.replace('did:near:', '');
     let publicKeyBase58List:string[] = [];
 
+    const networkId = this.getNetworkIdFromDid(did);
+
+    const network = this.networks[networkId];
+
+    if (!network) {
+      throw new Error(`Network configuration for '${networkId}' not found`);
+    }
+
     if (this.isNamedAccount(did)) {
       try {
-        publicKeyBase58List = await this.getPublicKeyFromRPC(identifier);
+        publicKeyBase58List = await this.getPublicKeyFromRPC(identifier, network);
       } catch (err) {
         throw new Error(`Named account ${identifier} not found: ${(err as Error).message}`);
       }
     } else if (this.isBase58Did(did)) {
-      const near = await this.getNear();
-      const account: Account = await near.account(this.CONTRACT_ID);
+      const near = await this.getNear(network);
+      const account: Account = await near.account(network.contractId);
 
       const owner: string | null = await account.viewFunction({
-        contractId: this.CONTRACT_ID,
+        contractId: network.contractId,
         methodName: 'identity_owner',
         args: { identity: did },
       });
 
       if (!owner || owner === 'null') {
-        throw new Error(`DID ${did} not registered in ${this.CONTRACT_ID}`);
+        throw new Error(`DID ${did} not registered in ${network.contractId} contract`);
       }
 
       publicKeyBase58List = [owner.startsWith('did:near:')
